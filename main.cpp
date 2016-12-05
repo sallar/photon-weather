5ed9522a4d723ac36850abeceed64d7f7e278a38
Salar Kaboli <sallar.kaboli@gmail.com>
2016-12-05 22:45:38 +0200

    Add time and temperature

--- main.cpp
+++ main.cpp
@@ -1,24 +1,35 @@
 #include "application.h"
 #include "LiquidCrystal/LiquidCrystal.h"
 
-// Make sure to update these to match how you've wired your pins.
-// pinout on LCD [RS, EN, D4, D5, D6, D7];
-// pin nums LCD  [ 4,  6, 11, 12, 13, 14];
-// Shield Shield [RS, EN, D4, D5, D6, D7];
-// Spark Core    [D3, D5, D2, D4, D7, D8];
+// Setup Crystal Display
 LiquidCrystal lcd(D0, D1, D2, D3, D4, D5);
 
 void setup() {
-  // set up the LCD's number of columns and rows: 
+  // Write
   lcd.begin(16,2);
-  // Print a message to the LCD.
-  lcd.print("Outside: -2");
+  lcd.setCursor(0, 0);
+  lcd.print("Inside:");
+  
+  // Set timezone to Helsinki
+  Time.zone(+2);
+  
+  // Set A0 Pinmode to input (temp sensor)
+  pinMode(A0, INPUT);
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+  // Read TMP sensor value
+  int analogValue = analogRead(A0); 
+  double voltage = 3.3 * ((double)analogValue / 4095.0);
+  int temperatureC = (voltage - 0.5) * 100;
+  
+  // Write temperature
+  lcd.setCursor(14, 0);
+  lcd.print(temperatureC);
+  
+  // Write time
   lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(Time.format("%d/%m %H:%M:%S"));
+  lcd.print(Time.format("%d/%m   %H:%M:%S"));
+  
+  delay(1000);
 }
